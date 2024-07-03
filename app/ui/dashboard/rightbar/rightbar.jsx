5b7dfddf57201fa24289f42259fd2af46d46428f
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2024-07-03 10:28:27 +0200

    implement dashboard mockup

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import styles from './rightbar.module.css'
+import Image from 'next/image'
+import { MdPlayCircleFilled, MdReadMore } from 'react-icons/md'
 
 const RightBar = () => {
   return (
-    <div>
-      Right Bar
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image 
+          className={styles.bg}
+            src='/student-notification.jpg'
+            alt='student notification'
+            fill
+          />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.notification}>🔥 Available Now</div>
+          <h3 className={styles.title}>How to use the new version of the admin dashboard</h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p>Empowering children through scholarships and food security</p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.texts}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions available, partial pre-rendering is coming up!
+          </h3>
+          <span className={styles.subtitle}>Boost Your Productivity!</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl ac
+            tincidunt tincidunt, nunc nunc ultrices nunc, nec aliquam nunc nunc in mauris.
+            Nulla facilisi. Sed euismod, nisl ac tincidunt tincidunt, nunc nunc ultrices
+            nunc, nec aliquam nunc nunc in mauris. Nulla facilisi.
+          </p>
+          <button className={styles.button}>
+           <MdReadMore />
+           Learn
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default RightBar
--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,13 +1,83 @@
 import React from 'react'
 import styles from './transactions.module.css'
+import Image from 'next/image'
 
 const Transactions = () => {
   return (
     <div className={styles.container}>
-      <h2>Latest Transactions</h2>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src='/noavatar.png'
+                  alt='user'
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.pending} ${styles.status}`}>Pending</span>
+            </td>
+            <td>01.07.2024</td>
+            <td>$450</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+
+                <Image
+                  src='/noavatar.png'
+                  alt='user'
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>01.07.2024</td>
+            <td>$450</td>
+          </tr> <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src='/noavatar.png'
+                  alt='user'
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.cancelled} ${styles.status}`}>Cancelled</span>
+            </td>
+            <td>01.07.2024</td>
+            <td>$450</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
 
 export default Transactions
- 
